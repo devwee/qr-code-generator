9a2bae2d9b37b5f190d9fed904322bbab2fdf148
Igor <111082011+devwee@users.noreply.github.com>
2023-09-25 08:18:13 -0300

    added color options for the user to generate the qr code they need

--- App.js
+++ App.js
@@ -10,11 +10,12 @@ import {
   KeyboardAvoidingView,
   Alert,
   Keyboard,
+  TouchableOpacity,
 } from "react-native";
 import { captureRef } from "react-native-view-shot";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import QRCode from "react-native-qrcode-svg";
-import domtoimage from 'dom-to-image';
+import domtoimage from "dom-to-image";
 
 import * as MediaLibrary from "expo-media-library";
 
@@ -25,14 +26,32 @@ export default function App() {
   const [inputText, setInputText] = useState(null);
   const [qrValue, setQrValue] = useState(null);
 
+  const [chosenColorOption, setChosenColorOption] = useState('white');
+  const [chosenBackgroundOption, setChosenBackgroundOption] = useState('transparent');
+
+  const colorOptions = ["Preto", "Branco"];
+  const backgroundOptions = ["Preto", "Branco", "Transparente"];
+
+  const colorTranslation = {
+    "Preto"  : "black",
+    "Branco" : "white",
+  };
+
+  const backgroundTranslation = {
+    "Preto"         : "black",
+    "Branco"        : "white",
+    "Transparente"  : "transparent",
+  };
+
   const onResetQRValue = () => {
     setQrValue(null);
     setInputText(null);
     if (Platform.OS === "ios" || Platform.OS === "android") {
-      Alert.alert("Sucesso!", "Dados resetados com sucesso.", [{ text: "OK" }]);
+      //Alert.alert("Sucesso!", "Dados resetados com sucesso.", [{ text: "OK" }]);
       Keyboard.dismiss();
     } else {
-      alert("Dados resetados com sucesso.");
+      setInputText('');
+      //alert("Dados resetados com sucesso.");
       Keyboard.dismiss();
     }
   };
@@ -52,12 +71,12 @@ export default function App() {
       setQrValue(inputText);
       Keyboard.dismiss();
       if (Platform.OS === "ios" || Platform.OS === "android") {
-        Alert.alert("Sucesso!", "Seu QR Code foi gerado com sucesso!", [
+        /*Alert.alert("Sucesso!", "Seu QR Code foi gerado com sucesso!", [
           { text: "OK" },
-        ]);
+        ]);*/
         Keyboard.dismiss();
       } else {
-        alert("Seu QR Code foi gerado com sucesso!");
+        //alert("Seu QR Code foi gerado com sucesso!");
         Keyboard.dismiss();
       }
     }
@@ -88,16 +107,29 @@ export default function App() {
       }
     } else {
       try {
-        const dataUrl = await domtoimage.toJpeg(imageRef.current, {
-          quality: 0.95,          
-          width: 250,
-          height: 250,
-        });
+        if(chosenBackgroundOption === 'transparent'){
+          const dataUrl = await domtoimage.toPng(imageRef.current, {
+            quality: 0.95,
+            width: 250,
+            height: 250,
+          });
 
-        let link = document.createElement("a");
-        link.download = "qr-code.png";
-        link.href = dataUrl;
-        link.click();
+          let link = document.createElement("a");
+          link.download = "qr-code.png";
+          link.href = dataUrl;
+          link.click();
+        } else {
+          const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+            quality: 0.95,
+            width: 250,
+            height: 250,
+          });
+  
+          let link = document.createElement("a");
+          link.download = "qr-code.jpeg";
+          link.href = dataUrl;
+          link.click();
+        }        
       } catch (e) {
         console.log(e);
       }
@@ -114,26 +146,23 @@ export default function App() {
       behavior={Platform.OS === "ios" ? "padding" : "height"}
       keyboardVerticalOffset={Platform.OS === "ios" ? 40 : 0}
     >
-      <View style={styles.containerQr}>
-        <Text style={styles.title}>Seu QR Code:</Text>
-        {qrValue ? (
-          <View ref={imageRef} collapsable={false}>            
+      {qrValue ? (
+        <View style={styles.containerQr}>
+          <Text style={styles.title}>Seu QR Code:</Text>
+          <View ref={imageRef} collapsable={false}>
             <QRCode
               value={qrValue}
               size={250}
-              color="white"
-              backgroundColor="transparent"
+              color={chosenColorOption}
+              backgroundColor={chosenBackgroundOption}
               logoSize={30}
-              logoBorderRadius={15}
-              logoBackgroundColor="transparent"
+              logoBorderRadius={15}              
             />
           </View>
-        ) : (
-          <Text style={styles.title}>
-            Preencha o campo abaixo e gere seu QR Code.
-          </Text>
-        )}
-      </View>
+        </View>
+      ) : (
+        ""
+      )}
 
       {qrValue ? (
         <View
@@ -165,6 +194,54 @@ export default function App() {
       )}
 
       <View style={styles.container}>
+        {qrValue ? (
+          <View>            
+          </View>
+        ) : (
+          <View>
+            <Text style={[styles.title, { textAlign: "center" }]}>
+              Escolha a cor do QR Code:
+            </Text>
+            <View style={styles.radioButtonContainer}>
+              {colorOptions.map((option, index) => (
+                <TouchableOpacity
+                  key={index}
+                  style={[
+                    styles.radioButton,
+                    {
+                      borderColor:
+                        chosenColorOption === colorTranslation[option] ? "gray" : "#2c2c2c",
+                    },                    
+                  ]}
+                  onPress={() => setChosenColorOption(colorTranslation[option])}
+                >
+                  <Text style={{ color: "#fff" }}>{option}</Text>
+                </TouchableOpacity>
+              ))}
+            </View>
+            <Text style={[styles.title, { textAlign: "center" }]}>
+              Escolha a cor de fundo do QR Code:
+            </Text>
+            <View style={styles.radioButtonContainer}>
+              {backgroundOptions.map((option, index) => (
+                <TouchableOpacity
+                  key={index}
+                  style={[
+                    styles.radioButton,
+                    {
+                      borderColor:
+                        chosenBackgroundOption === backgroundTranslation[option] ? "gray" : "#2c2c2c",
+                    },
+                  ]}
+                  onPress={() => setChosenBackgroundOption(backgroundTranslation[option])}
+                >
+                  <Text style={{ color: "#fff" }}>{option}</Text>
+                </TouchableOpacity>
+              ))}
+            </View>
+          </View>
+        )}
+
         <Text style={styles.text}>Insira seu texto e/ou URL:</Text>
         <TextInput
           style={styles.textInput}
@@ -216,8 +293,9 @@ export default function App() {
             />
             <Text style={styles.buttonLabel}>Resetar QR Code</Text>
           </Pressable>
-        </View>
-      </View>
+        </View>        
+        <Text style={{ textAlign: 'center', marginTop: 5, color: 'white', fontSize: 12 }}><FontAwesome name="github" color="#fff" size={12}/> feito por Dev Wee (Igor)</Text>
+      </View>      
       <StatusBar style="light" />
     </KeyboardAvoidingView>
   );
@@ -290,5 +368,17 @@ const styles = StyleSheet.create({
   buttonLabel: {
     fontSize: 16,
     color: "#fff",
+  },
+  radioButtonContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  radioButton: {
+    marginRight: 10,
+    marginBottom: 25,
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 5,
   },
 });
